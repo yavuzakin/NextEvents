b19a6d019a7f225561a249eb6c757c44f2390650
yavuzakin <yavuzakin55@gmail.com>
2022-01-09 16:19:31 +0300

    Modified global css and home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+
 function FeaturedEventsPage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>Featured Events Page</h1>
+      <EventList events={featuredEvents} />
     </div>
   );
 }
