423d945ffb859842dc9d5dc91f5626b9bc4a2a52
yavuzakin <yavuzakin55@gmail.com>
2022-01-09 17:40:23 +0300

    events-search component added into all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 function AllEventsPage() {
   const events = getAllEvents();
   return (
     <Fragment>
+      <EventsSearch />
       <EventList events={events} />;
     </Fragment>
   );
