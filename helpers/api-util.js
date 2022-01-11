a28f4d1c5a7952b37433baf4f67bbe41bbd9f256
yavuzakin <yavuzakin55@gmail.com>
2022-01-11 15:12:06 +0300

    Added helper functions to fetch data from firebase for event detail page

--- helpers/api-util.js
+++ helpers/api-util.js
@@ -16,3 +16,8 @@ export async function getFeaturedEvents() {
   const events = await getAllEvents();
   return events.filter((event) => event.isFeatured);
 }
+
+export async function getEventById(id) {
+  const events = await getAllEvents();
+  return events.find((event) => event.id === id);
+}
