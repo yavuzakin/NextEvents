653529ca195ca5d8de2091b0fcb8ed21949df097
yavuzakin <yavuzakin55@gmail.com>
2022-01-11 15:22:32 +0300

    Modified all events page so that it does SSG and revalidates in every minute

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,11 @@
 import { Fragment } from "react";
 import { useRouter } from "next/router";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 
-function AllEventsPage() {
-  const events = getAllEvents();
+function AllEventsPage(props) {
+  const { events } = props;
   const router = useRouter();
 
   function findEventsHandler(year, month) {
@@ -22,3 +22,13 @@ function AllEventsPage() {
 }
 
 export default AllEventsPage;
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
+}
