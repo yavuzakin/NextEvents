96698d1e78cccf41c80b798dfa7c8bde25535a84
yavuzakin <yavuzakin55@gmail.com>
2022-01-11 15:12:46 +0300

    Modified event detail page so that it does SSG

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,15 +1,12 @@
-import { useRouter } from "next/router";
 import { Fragment } from "react";
 import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
 import ErrorAlert from "../../components/ui/error-alert";
-import { getEventById } from "../../dummy-data";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
-function EventDetailPage() {
-  const router = useRouter();
-  const id = router.query.id;
-  const event = getEventById(id);
+function EventDetailPage(props) {
+  const event = props.event;
 
   if (!event) {
     return (
@@ -36,3 +33,23 @@ function EventDetailPage() {
 }
 
 export default EventDetailPage;
+
+export async function getStaticProps(context) {
+  const { id } = context.params;
+  const event = await getEventById(id);
+  return {
+    props: {
+      event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths() {
+  const featuredEvents = await getFeaturedEvents();
+  const paths = featuredEvents.map((event) => ({ params: { id: event.id } }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+}
