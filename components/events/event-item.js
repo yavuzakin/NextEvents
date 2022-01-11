7b3121ca81bcf207780570b145e1d10b7cedaf02
yavuzakin <yavuzakin55@gmail.com>
2022-01-11 20:18:22 +0300

    img elements replaced by Image of next for optimization

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Button from "../ui/button";
 import styles from "./event-item.module.css";
 import AddressIcon from "../icons/address-icon";
@@ -15,7 +16,7 @@ function EventItem(props) {
   const exploreLink = `/events/${id}`;
   return (
     <li className={styles.item}>
-      <img src={`/${image}`} alt={title} />
+      <Image src={`/${image}`} alt={title} width={250} height={160} />
       <div className={styles.content}>
         <div className={styles.summary}>
           <h2>{title}</h2>
